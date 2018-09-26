1af17d060b3814ed9ca5e795220abd497d045232
Liam Coghill <liam.coghill@deepend.melbourne>
2018-09-26 16:39:20 +1000

    Add string extensions for Base64 encoding and decoding

--- code/Extensions/StringExtensions.cs
+++ code/Extensions/StringExtensions.cs
@@ -1,4 +1,4 @@
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Sitecore.Foundation.SitecoreExtensions.Extensions
 {
@@ -12,6 +12,38 @@ namespace Sitecore.Foundation.SitecoreExtensions.Extensions
         public static string ToCssUrlValue(this string url)
         {
             return string.IsNullOrWhiteSpace(url) ? "none" : $"url('{url}')";
+        }
+
+        /// <summary>
+        /// Encodes the provided string to Base64
+        /// </summary>
+        /// <param name="plainText">UTF8 string</param>
+        /// <returns>plainText encoded to a Base64 string</returns>
+        public static string EncodeToBase64(this string plainText)
+        {
+            if (plainText == null)
+            {
+                return null;
+            }
+
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        /// <summary>
+        /// Decodes the provided string from Base64 to plain text
+        /// </summary>
+        /// <param name="encodedText">Base64 string</param>
+        /// <returns>encodedText decoded to a plain text UTF8 string</returns>
+        public static string DecodeFromBase64(this string encodedText)
+        {
+            if (encodedText == null)
+            {
+                return null;
+            }
+
+            var base64EncodedBytes = System.Convert.FromBase64String(encodedText);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
     }
 }
